16b0010096bc600429566fff8dd6ab2f98b6094c
Krista Calderon <kamsci@gmail.com>
2016-10-27 10:45:52 -0700

    added model file with mongoose schema for job

---
+++ models/job.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var jobSchema = new Schema({
+    title: { type: String, required: true },
+    referenceId: { type: String, required: true, unique: true },
+    jobComplete: { type: Date },
+    stops: [{
+        stopType: { type: String },
+        cargoDescription: { type: String },
+        arrivalTime: { type: Date },
+        address: { type: String },
+        picture: { type: String },
+        ladingReceived?: { type: Date },
+        ladingSigned?: { type: Date }
+    }]
+});
+
+jobSchema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('Job', jobSchema);
